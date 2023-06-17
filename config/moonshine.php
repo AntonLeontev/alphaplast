4eb11af6f1c887a18293808b1cb5515fde4a6e93
Anton <aner-anton@ya.ru>
2023-06-17 10:13:54 +0300

    Logo admin

--- config/moonshine.php
+++ config/moonshine.php
@@ -8,7 +8,7 @@ return [
     'namespace' => 'App\MoonShine',
 
     'title' => env('MOONSHINE_TITLE', 'Альфапласт'),
-    'logo' => '/storage/images/logo-footer.png',
+    'logo' => '/images/logo-footer.png',
     'logo_small' => env('MOONSHINE_LOGO_SMALL'),
 
     'route' => [
